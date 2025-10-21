746803185b8bc350a60e5039add0faca33dcc4db
MazyVermoh <muslim.bagaev2017@gmail.com>
2025-10-21 09:56:55 +0300

    icon + name update

--- app.config.js
+++ app.config.js
@@ -2,17 +2,23 @@ import "dotenv/config";
 
 export default ({ config }) => ({
   ...config,
-  name: config.name ?? "Quadrant Mobile",
+  name: "Quadrant",
   slug: config.slug ?? "quadrant-mobile",
   version: config.version ?? "1.0.0",
   scheme: config.scheme ?? "quadrant",
+  icon: "./assets/icon_quadrant.png",
   android: {
     ...config.android,
-    package: config.android?.package ?? "com.muslim.quadrant"
+    package: config.android?.package ?? "com.muslim.quadrant",
+    adaptiveIcon: {
+      foregroundImage: "./assets/logo_dark.png",
+      backgroundColor: config.android?.adaptiveIcon?.backgroundColor ?? "#000000"
+    }
   },
   ios: {
     ...config.ios,
-    bundleIdentifier: config.ios?.bundleIdentifier ?? "com.muslim.quadrant"
+    bundleIdentifier: config.ios?.bundleIdentifier ?? "com.muslim.quadrant",
+    icon: "./assets/icon_quadrant.png"
   },
   extra: {
     ...config.extra,
